306bee0bb8bc2c79c9139e7c13c80ccb2f20632e
wanderson <wanderson.alves.rodrigues@gmail.com>
2020-12-30 11:50:42 -0300

    feat: index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-const test = 'ssss';
+import CeiService from './services/cei.service';
+
+(async () => {
+  const ceiService: CeiService = new CeiService({
+    login: '47782704014',
+    password: 'ewqqw324234',
+  });
+
+  await ceiService.loginAsync();
+})();
