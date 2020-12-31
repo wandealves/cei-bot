13395a3019f343d6fd80b71cafb300b3027b3923
wanderson <wanderson.alves.rodrigues@gmail.com>
2020-12-31 17:29:09 -0300

    feat:create transforme service

---
+++ src/services/transformer.service.ts
@@ -0,0 +1,22 @@
+import Transforme from '../constant/transforme';
+
+export default class TransformeService {
+  public static fromTo(tag: string): string {
+    if (!tag) return tag;
+
+    if (Transforme.TO.ACTIVE === tag) return Transforme.FROM.ACTIVE;
+    if (Transforme.TO.SPECIFICATION === tag || Transforme.TO.SPEC === tag)
+      return Transforme.FROM.SPECIFICATION;
+    if (Transforme.TO.CODE === tag) return Transforme.FROM.CODE;
+    if (Transforme.TO.PAYMENTDATE === tag || Transforme.TO.CREDITDATE === tag)
+      return Transforme.FROM.PAYMENTDATE;
+    if (Transforme.TO.EVENTTYPE === tag) return Transforme.FROM.EVENTTYPE;
+    if (Transforme.TO.QUANTItY === tag) return Transforme.FROM.QUANTItY;
+    if (Transforme.TO.QUOTATIONFACTOR === tag)
+      return Transforme.FROM.QUOTATIONFACTOR;
+    if (Transforme.TO.GROSSVALUE === tag) return Transforme.FROM.GROSSVALUE;
+    if (Transforme.TO.NETVALUE === tag) return Transforme.FROM.NETVALUE;
+
+    return tag;
+  }
+}
