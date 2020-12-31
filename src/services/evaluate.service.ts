65232d5c4c546136dd78195d8906f39e7d78c042
wanderson <wanderson.alves.rodrigues@gmail.com>
2020-12-31 17:28:42 -0300

    feat: create evaluate service

---
+++ src/services/evaluate.service.ts
@@ -0,0 +1,51 @@
+import puppeteer from 'puppeteer';
+
+import Item from '../models/item';
+
+export default class EvaluateService {
+  public static async getItemsAsync(
+    page: puppeteer.Page,
+    tag: string,
+  ): Promise<Item[]> {
+    const items: Item[] = await page.evaluate(selector => {
+      return Array.prototype.map.call(
+        document.querySelector(selector).children,
+        el => ({
+          id: el.value,
+          name: el.textContent.trim(),
+        }),
+      ) as Item[];
+    }, tag);
+
+    return items;
+  }
+
+  public static async getListTextAsync(
+    page: puppeteer.Page,
+    tag: string,
+  ): Promise<string[]> {
+    const rows = await page.evaluate(selector => {
+      return Array.prototype.map.call(document.querySelectorAll(selector), el =>
+        el.textContent.trim(),
+      ) as string[];
+    }, tag);
+
+    return rows;
+  }
+
+  public static async getRowsAsync(
+    page: puppeteer.Page,
+    tag: string,
+  ): Promise<string[]> {
+    const rows = await page.evaluate(selector => {
+      return Array.prototype.map.call(document.querySelectorAll(selector), el =>
+        Array.prototype.map.call(
+          el.children,
+          subEl => (subEl && subEl.textContent.trim()) || ``,
+        ),
+      ) as string[];
+    }, tag);
+
+    return rows;
+  }
+}
