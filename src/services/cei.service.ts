e337aba80c874dc1a00f60e878bf23b47b1207a0
Vinícius G. Oliveira <vinicius.oliver@gmail.com>
2021-08-30 07:50:27 -0300

    Update cei.service.ts
    Add linha args, problemas encontrados para rodar em servidor sem interface grafica.

--- src/services/cei.service.ts
+++ src/services/cei.service.ts
@@ -119,9 +119,10 @@ export class CeiService {
     }
   }
 
-  private async startAsync(setting: Setting): Promise<void> {
+  private async startAsync(setting?: Setting): Promise<void> {
     this.browser = await puppeteer.launch({
-      headless: setting.headless || Config.HEADLESS,
+      headless: setting?.headless || Config.HEADLESS,
+      args: ['--no-sandbox']
     });
     this.page = await this.browser.newPage();
   }
